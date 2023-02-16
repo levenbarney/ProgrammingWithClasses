ad292943a582af9b02ae79284bf6f554d91cbd2a
levenbarney <113639602+levenbarney@users.noreply.github.com>
2023-02-16 00:11:38 -0700

    updated scripture memorizer

--- ScriptureMemorizer/ScriptureMemorizer.cs
+++ ScriptureMemorizer/ScriptureMemorizer.cs
@@ -19,6 +19,35 @@ class ScriptureMemorizer
 
     public void removeWordsFromText()
     {
-        int numWordsToRemove = new
+        int numWordsToRemove = new Random().Next(2,4);
+        int wordsRemoved = 0;
+
+            do
+            {
+                int rndIndex= new Random().Next(0, scriptureTextList.Count());
+                scriptureTextList[rndIndex] = new string('_', scriptureTextList[rndIndex].Length);
+                wordsRemoved++;
+
+            }while (wordsRemoved!= numWordsToRemove);
+    }
+
+    public string ToString()
+    {
+        return string.Join(' ', scriptureTextList);
+    }
+
+    public bool hasWordsLeft()
+    {
+        bool retvalue = false;
+
+        foreach (string word in scriptureTextList)
+        {
+            if (word.Contains('_') == false)
+            {
+                retvalue - true;
+                break;
+            }
+        }
+        return retvalue;
     }
 }
